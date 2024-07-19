977582a8f7fab74c376fbefd514ee85e78928c72
Tech Ferry <vijayamuralikrishna>
2024-07-19 15:44:47 +0530

    Add Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,59 @@
-import { AppBar } from '@mui/material';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import "./navBar.css"
 
-const NavBar = () => {
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function NavBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <AppBar>NavBar</AppBar>
-  )
+    <AppBar position="static">
+        <Toolbar disableGutters >
+            <img src='https://stage-secure.realsatisfied.com/version/24.38/images/logo_on_charcoal_196_30.png'/>
+            <Toolbar>
+              <MenuItem>
+              Dashboard
+              </MenuItem>
+              <MenuItem>
+              Dashboard
+              </MenuItem>
+              <MenuItem>
+              Dashboard
+              </MenuItem>
+              <MenuItem>
+              Dashboard
+              </MenuItem>
+            </Toolbar>
+        </Toolbar>
+    </AppBar>
+  );
 }
-
-export default NavBar
+export default NavBar;
